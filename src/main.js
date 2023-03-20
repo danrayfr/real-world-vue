06c780cbd50cdcc1e814659725baf0a628c9e9a7
danrayfr <danrayrollan98@gmail.com>
2023-03-21 03:40:18 +0800

    real world vue resources 2

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './assets/global.css'
 
-createApp(App).use(store).use(router).mount('#app')
+// Global Components
+import BaseIcon from '@/components/BaseIcon'
+
+createApp(App).use(store).use(router).component('BaseIcon', BaseIcon).mount('#app')
---
+++ src/views/EventList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Events Listing</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event"/>
+  </div>
+</template>
+
+<script>
+import EventService from '@/services/EventService'
+import EventCard from '@/components/EventCard'
+export default {
+  components: { EventCard },
+  data() {
+    return { 
+      events: []
+    } 
+  },
+  created() {
+    EventService.getEvents()
+      .then(response => {
+        this.events = (response.data)
+      })
+      .catch(error => {
+        console.log("An error occurred: " + error.response)
+      })
+  } 
+}
+</script>
