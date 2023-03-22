812fbb881da719976d6db8d086477f21bed0ca44
danrayfr <danrayrollan98@gmail.com>
2023-03-22 17:38:21 +0800

    error handling, implement 404&network-issue route

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import EventShow from '@/views/EventShow'
 import EventCreate from '@/views/EventCreate'
 import NProgress from 'nprogress'
 import store from '@/store/index'
+import NotFound from '@/views/NotFound'
+import NetWorkIssue from '@/views/NetworkIssue'
 
 const routes = [
   {
@@ -22,6 +24,14 @@ const routes = [
         to.params.event = event
         next()
       })
+      .catch(error => {
+        if (error.response && error.response.status == 404) {
+          next({ name:'404', params: { resource: 'event'} })
+        } else { 
+          next({ name: 'network-issue' })
+        }
+      })
+    
     }
   },
 
@@ -29,6 +39,21 @@ const routes = [
     path: '/event/create',
     name: 'event-create',
     component: EventCreate
+  },
+  {
+    path: '/404/:resource',
+    name: '404',
+    component: NotFound,
+    props: true,
+  },
+  {
+    path: '/network-issue',
+    name: 'network-issue',
+    component: NetWorkIssue
+  }, 
+  {
+    path: '/:pathMatch(.*)*',
+    redirect: { name: '404', params: { resource: 'page'}}, 
   }
 ]
 
--- src/store/modules/event.js
+++ src/store/modules/event.js
@@ -65,7 +65,7 @@ export const actions = {
         dispatch('notification/add', notification, { root: true})
       })
   },
-  fetchEvent({commit, dispatch, getters}, id) {
+  fetchEvent({commit, getters}, id) {
     var event = getters.getEventById(id)
     if (event) {
       commit('SET_EVENT', event)
@@ -75,13 +75,6 @@ export const actions = {
         .then(response => {
           commit('SET_EVENT', response.data)
           return response.data
-        })
-        .catch(error => {
-          const notification = { 
-            type: 'error',
-            message: 'There was an error fetching event: ' + error.message
-          }
-          dispatch('notification/add', notification, { root: true})
         })
     }
   }
